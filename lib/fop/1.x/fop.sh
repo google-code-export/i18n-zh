0480897582ed9a968280ee2c1a74150c783f3fe2
saimei.su <saimei.su@0ccedc20-662c-0410-b999-856e3c85085e>
2007-05-15 12:59:42 +0000

    Add fop trunk@r537129

---
+++ lib/fop/1.x/fop.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME" ] ; then
+    JAVACMD="$JAVA_HOME/bin/java"
+  else
+    JAVACMD=`which java 2> /dev/null`
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly."
+  echo "  We cannot execute java"
+  exit 1
+fi
+
+[ -z "$FOP_HOME" ] && FOP_HOME=`dirname $0`
+
+if [ -z "$FOP_HOME" -o ! -d "$FOP_HOME" ] ; then
+  ## resolve links - $0 may be a link to fop's home
+  PRG="$0"
+  progname=`basename "$0"`
+
+  # need this for relative symlinks
+  while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+      PRG="$link"
+    else
+      PRG=`dirname "$PRG"`"/$link"
+    fi
+  done
+
+  FOP_HOME=`dirname "$PRG"`
+
+  # make it fully qualified
+  FOP_HOME=`cd "$FOP_HOME" && pwd`
+fi
+
+# add fop.jar, fop-sandbox and fop-hyph.jar, which reside in $FOP_HOME/build
+LOCALCLASSPATH=$FOP_HOME/build/fop.jar:$FOP_HOME/build/fop-hyph.jar:$FOP_HOME/build/fop-sandbox.jar
+
+# add in the dependency .jar files, which reside in $FOP_HOME/lib
+for i in ${FOP_HOME}/lib/*.jar; do
+  LOCALCLASSPATH="$i":$LOCALCLASSPATH
+done
+
+LOGCHOICE=-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog
+LOGLEVEL=-Dorg.apache.commons.logging.simplelog.defaultlog=WARN
+
+$JAVACMD -Xmx512m $LOGCHOICE $LOGLEVEL -classpath $LOCALCLASSPATH org.apache.fop.cli.Main $@
