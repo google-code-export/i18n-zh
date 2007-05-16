a0dbe4ace7b06f201524eb343d3552334fad0362
saimei.su <saimei.su@0ccedc20-662c-0410-b999-856e3c85085e>
2007-05-16 02:32:29 +0000

    Fix fop.cmd

--- trunk/lib/fop/0.20.5/fop.cmd
+++ trunk/lib/fop/0.20.5/fop.cmd
@@ -1,19 +1,26 @@
-@set FOP_HOME=%~dp0
+@ECHO OFF
 
-@if exist "%JAVA_HOME%\lib\tools.jar" goto okJH
-@echo The JAVA_HOME environment variable is not defined correctly
-@goto end
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%JAVACMD%" == "" set JAVACMD=%JAVA_HOME%\bin\java
+goto runFop
 
-:okJH
+:noJavaHome
+if "%JAVACMD%" == "" set JAVACMD=java
 
-@if exist "%FOP_HOME%\lib\batik.jar" goto okAH
-@echo The FOP_HOME environment variable is not defined correctly
-@goto end
+:runFop
 
-:okAH
+set LOCAL_FOP_HOME=%~dp0
+set LIBDIR=%LOCAL_FOP_HOME%lib
 
-for %%l in (%FOP_HOME%\lib\*.jar) do set LOCALCLASSPATH=!LOCALCLASSPATH!;%%l
+set LOCALCLASSPATH=%LIBDIR%\fop-0.20.5-RFC3066-patched.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\avalon-framework-cvs-20020806.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\batik.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jai_core.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jai_codec.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\rowan-0.1.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\saxon.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\saxoncatalog.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\saxon-dbxsl-extensions.jar
 
-@"%JAVA_HOME%\bin\java.exe" -Xmx512m -cp "%LOCALCLASSPATH%" org.apache.fop.apps.Fop %*
-
-:end
+"%JAVACMD%" -Xmx512m -cp "%LOCALCLASSPATH%" org.apache.fop.apps.Fop %*
