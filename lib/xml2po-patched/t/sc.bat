641a370d8421d6e77514e6929471670d3ae9bd60
dongsheng.song <dongsheng.song@0ccedc20-662c-0410-b999-856e3c85085e>
2007-10-09 11:55:07 +0000

    Add xml2po-patched.

---
+++ lib/xml2po-patched/t/sc.bat
@@ -0,0 +1,6 @@
+python xml2po.py -o sc.pot sc.xml
+
+msgmerge --no-wrap -o sc-zh_CN_new.po sc-zh_CN.po sc.pot
+mv -f sc-zh_CN_new.po sc-zh_CN.po
+
+python xml2po.py -o sc-zh_CN.xml -p sc-zh_CN.po sc.xml
