fc6bd66d2088d0c0166e786c8e9777ffff9dfa7a
dongsheng.song <dongsheng.song@0ccedc20-662c-0410-b999-856e3c85085e>
2008-06-27 01:31:25 +0000

    Add CmapHeader

---
+++ trunk/lib/tools/otf/src/otf/pojo/ByteEncodingTable.java
@@ -0,0 +1,8 @@
+package otf.pojo;
+
+public class ByteEncodingTable {
+    public short format;
+    public short length;
+    public short language;
+    public byte glyphIdArray[] = new byte[256];
+}
---
+++ trunk/lib/tools/otf/src/otf/pojo/EncodingRecord.java
@@ -0,0 +1,7 @@
+package otf.pojo;
+
+public class EncodingRecord {
+    public short platformID;
+    public short encodingID;
+    public int offset;
+}
