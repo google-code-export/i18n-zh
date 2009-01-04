fb22d0d61e1438032c42caa0d47c067727dd75ee
dongsheng.song <dongsheng.song@0ccedc20-662c-0410-b999-856e3c85085e>
2009-01-04 09:56:56 +0000

    xep-4.14-20081212-personal.zip

---
+++ lib/xep/xep.bat
@@ -0,0 +1,25 @@
+@echo off
+rem   This batch file encapsulates a standard XEP call.
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%JAVACMD%" == "" set JAVACMD=%JAVA_HOME%\bin\java
+goto runXep
+
+:noJavaHome
+if "%JAVACMD%" == "" set JAVACMD=java
+
+:runXep
+@set XEP_HOME=%~dp0..
+
+set CP=%XEP_HOME%\lib\xep.jar;%XEP_HOME%\lib\saxon.jar;%XEP_HOME%\lib\xt.jar
+
+if x%OS%==xWindows_NT goto WINNT
+"%JAVACMD%" -Xmx1024m -classpath "%CP%" com.renderx.xep.XSLDriver "-DCONFIG=%XEP_HOME%\xep.xml" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto END
+
+:WINNT
+"%JAVACMD%" -Xmx1024m -classpath "%CP%" com.renderx.xep.XSLDriver "-DCONFIG=%XEP_HOME%\xep.xml" %*
+
+:END
+set CP=
