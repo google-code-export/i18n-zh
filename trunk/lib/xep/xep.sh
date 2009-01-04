e641f9015ae7de48308d7b3ed7ac48f21fc8712e
dongsheng.song <dongsheng.song@0ccedc20-662c-0410-b999-856e3c85085e>
2009-01-04 09:56:56 +0000

    xep-4.14-20081212-personal.zip

---
+++ trunk/lib/xep/xep.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ -z "$JAVACMD" ]; then
+    if [ ! -z "$JAVA_HOME" ]; then
+        if [ ! -e "$JAVA_HOME/bin/java" ]; then
+            JAVACMD=java
+        else
+            JAVACMD=$JAVA_HOME/bin/java
+        fi
+    else
+        JAVACMD=java
+    fi
+fi
+
+echo "JAVACMD: $JAVACMD"
+
+[ -z "$XEP_HOME" ] && XEP_HOME=`dirname $0`/..
+if [ ! -e "$XEP_HOME/lib/xep.jar" ]
+then
+    echo The XEP_HOME environment variable is not defined correctly
+    exit 1
+fi
+
+$JAVACMD -Xmx1024m "-DCONFIG=%XEP_HOME%/xep.xml" \
+    -cp $XEP_HOME/lib/xep.jar:$XEP_HOME/lib/saxon.jar:$XEP_HOME/lib/xt.jar  \
+    org.apache.tools.XEP.launch.Launcher -lib $XEP_HOME/lib "$@"
