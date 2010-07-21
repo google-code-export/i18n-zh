80f15003c36a520d800b53c2d6cd2155d1d9b391
dongsheng.song@gmail.com <dongsheng.song@gmail.com@0ccedc20-662c-0410-b999-856e3c85085e>
2010-07-21 09:25:46 +0000

    [fop] Update to 1.0

--- trunk/lib/fop/fop.bat
+++ trunk/lib/fop/fop.bat
@@ -7,20 +7,17 @@ set LOGCHOICE=-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.S
 set LOGLEVEL=-Dorg.apache.commons.logging.simplelog.defaultlog=WARN
 
 set LOCALCLASSPATH=%LOCAL_FOP_HOME%build\fop.jar
-set LOCALCLASSPATH=%LOCALCLASSPATH%;%LOCAL_FOP_HOME%build\fop-sandbox.jar
-set LOCALCLASSPATH=%LOCALCLASSPATH%;%LOCAL_FOP_HOME%build\fop-hyph.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\avalon-framework-4.2.0.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\batik-all-1.7.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-io-1.3.1.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-logging-1.1.1.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\serializer-2.7.1.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xalan-2.7.1.jar
-set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xalan-dbxsl.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xercesImpl-2.9.1.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis-1.3.04.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis-ext-1.3.04.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-commons-resolver-1.2.jar
-set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xmlgraphics-commons-1.4svn.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xmlgraphics-commons-1.4.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%FOP_HYPHENATION_PATH%
 
 if "%JAVA_HOME%" == "" goto noJavaHome
